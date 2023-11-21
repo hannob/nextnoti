2239f0a111b5c8b0cfceba7a8bb83485e989e9f3
Hanno Böck <990588+hannob@users.noreply.github.com>
2023-11-21 13:37:24 +0100

    avoid trailing comma

--- nextnoti.php
+++ nextnoti.php
@@ -15,15 +15,19 @@ $calid = isset($c_calendarid) ? (int)$c_calendarid : 1;
 $out = $db->query("SELECT calendardata FROM oc_calendarobjects WHERE calendarid=$calid");
 
 $mc = '';
-$ms = '';
+$ms = [];
 foreach ($out as $row) {
     $s = $row['calendardata'];
     if (preg_match('/DTSTART.*' . date('Ymd') . '/', $s)) {
         preg_match('/SUMMARY[^:]*:(.*)/', $s, $m);
-        $ms .= $m[1] . ',';
+        if (count($m) < 2) {
+            echo "WARNING: Calendar entry has no SUMMARY";
+        }
+        array_push($ms, $m[1]);
         $mc .= $s;
     }
 }
+$ms = implode(",", $ms);
 
 $ms = mb_encode_mimeheader($ms, 'UTF-8', 'Q');
 if ($ms != '') {
