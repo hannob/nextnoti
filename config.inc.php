ca6ca9a34c1beac4dd5876da7ebcedc22a6cbf95
Hanno Böck <990588+hannob@users.noreply.github.com>
2025-07-04 11:17:48 +0200

    use phpmailer with smpt authentication, rename some config variables for consistency with my other scripts

--- config.inc.php
+++ config.inc.php
@@ -1,11 +1,14 @@
 <?php
 
 // Set mail address
-$c_mailto = 'to@example.org';
-$c_mailfrom = 'from@example.org';
+$_mail_to = 'to@example.org';
+$_mail_from = 'from@example.org';
+$_mail_server = "mail.example.org";
+$_mail_user = "exampleuser";
+$_mail_pass = "examplepass";
 
 // Calendar id in database (optional, default: 1)
-// $c_calendarid = 1;
+$c_calendarid = 1;
 
 // Path to sqlite db
-$c_sqlitefile = dirname(__FILE__) . '/../data/owncloud.db';
+$_sqlitefile = dirname(__FILE__) . '/../data/owncloud.db';
--- nextnoti.php
+++ nextnoti.php
@@ -1,6 +1,12 @@
 #!/usr/bin/php
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once('vendor/autoload.php');
+
 $DEBUG = false;
 if ((count($argv) > 1) && ($argv[1] == "-d")) {
     $DEBUG = true;
@@ -8,14 +14,14 @@ if ((count($argv) > 1) && ($argv[1] == "-d")) {
 
 require_once(dirname(__FILE__) . "/config.inc.php");
 
-if (!$c_mailto) {
-    print('You need to set $c_mailto in config.inc.php!' . "\n");
+if (!$_mail_to) {
+    print('You need to set $_mail_to in config.inc.php!' . "\n");
     exit(1);
 }
 
-$db = new PDO('sqlite:' . $c_sqlitefile);
+$db = new PDO('sqlite:' . $_sqlitefile);
 
-$calid = isset($c_calendarid) ? (int) $c_calendarid : 1;
+$calid = isset($_calendarid) ? (int) $_calendarid : 1;
 
 $out = $db->query("SELECT calendardata FROM oc_calendarobjects WHERE calendarid=$calid");
 
@@ -40,7 +46,29 @@ foreach ($out as $row) {
 }
 $ms = implode(",", $ms);
 
-$ms = mb_encode_mimeheader($ms, 'UTF-8', 'Q');
 if ($ms != '') {
-    mail($c_mailto, $ms, $mc, ["From" => $c_mailfrom, "Content-Type" => "text/plain;charset=utf-8"]);
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Username = $_mail_user;
+        $mail->Password = $_mail_pass;
+        $mail->Host = $_mail_server;
+        $mail->SMTPAuth = true;
+        $mail->Port = 465;
+        $mail->CharSet = PHPMailer::CHARSET_UTF8;
+
+        $mail->setFrom($_mail_from);
+        $mail->addAddress($_mail_to);
+
+        $mail->Subject = $ms;
+        $mail->Body = $mc;
+
+        $mail->send();
+    } catch (Exception $e) {
+        echo "An error happened...\n";
+        mail($_mail_from, "Mailerror nextnoti", $mail->ErrorInfo . "\n\n" . $e->getMessage());
+        die();
+    }
+
 }
